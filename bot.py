63095e3867ebe94cf08bf097ff5c0a239b136c91
KAZAK-2917 <maks1mkatitov@yandex.ru>
2025-07-28 10:46:45 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
-import os
+iimport os
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 
-API_TOKEN = os.getenv('TOKEN')
-TARGET_CHAT = os.getenv('TARGET_CHAT')
+API_TOKEN = os.getenv("API_TOKEN")
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
+
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
